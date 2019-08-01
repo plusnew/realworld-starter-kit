021ae6b3f51044ad98f720a6742b2e536c9f9861
plusgut <github@webentwickler2-0.de>
2019-08-01 09:14:46 +0200

    [navigation] added navigation information

--- src/App/components/Header/index.tsx
+++ src/App/components/Header/index.tsx
@@ -2,5 +2,28 @@ import plusnew, { component } from 'plusnew';
 
 export default component(
   __dirname,
-  () => <nav />,
+  () =>
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <a className="navbar-brand" href="index.html">conduit</a>
+        <ul className="nav navbar-nav pull-xs-right">
+          <li className="nav-item">
+        <a className="nav-link active" href="">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="">
+              <i className="ion-compose"></i>&nbsp;New Post
+        </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="">
+              <i className="ion-gear-a"></i>&nbsp;Settings
+        </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="">Sign up</a>
+          </li>
+        </ul>
+      </div>
+    </nav>,
 );
